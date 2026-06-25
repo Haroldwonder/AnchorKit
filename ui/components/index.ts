db4c6dc67557762bda442474c841903b7c135da0
onlywiseone <wie323032@gmail.com>
2026-06-25 23:50:57 +0000

    feat(#680): export TransactionTimeline with click-to-expand step panels from index.ts

--- ui/components/index.ts
+++ ui/components/index.ts
@@ -6,3 +6,6 @@
 
 export { ApiRequestPanel } from './ApiRequestPanel';
 export type { ApiRequestPanelProps } from './ApiRequestPanel';
+
+export { TransactionTimeline } from './TransactionTimeline';
+export type { TransactionTimelineProps, TxEvent, TxStatus, TxType } from './TransactionTimeline';
